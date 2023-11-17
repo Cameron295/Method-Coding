d270509493681deb66fb27e793dd79008748e04a
Cameron295 <149431527+Cameron295@users.noreply.github.com>
2023-11-16 21:44:11 -0600

    Update User.py

--- User.py
+++ User.py
@@ -1,1 +1,19 @@
+class User:
 
+    def __init__(self, databaseName, tableName, loggedIn, userID):
+        self.databaseName = databaseName
+        self.tableName = tableName
+        self.loggedIn = loggedIn
+        self.userID = userID
+
+    def login():
+
+    def logout():
+
+    def viewAccountInformation():
+
+    def createAccount():
+
+    def getLoggedIn():
+
+    def getUserID():
