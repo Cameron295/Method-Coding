bc080fa7012e04dec1822d91ea96cf281e51fd90
Micah George <149753394+GeorgeMicah24@users.noreply.github.com>
2023-11-17 14:57:02 -0800

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -4,7 +4,11 @@ class Inventory:
         self.tableName = tableName
 
     def viewInventory():
+    # displays all items currently in the inventory
         
     def searchInventory():
+    # asks for a title from the user, then checks for any matches in the database and returns the result
+    # if it's a successful search, displays all results to user. If unsuccessful, informs the user their search failed.
 
     def decreasedStock(ISBN):
+    # decreases stock number of the given ISBN's respective item
