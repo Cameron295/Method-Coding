ffac5f7c963f876cd3ccf278c8e9d0447d8491e0
Cameron295 <149431527+Cameron295@users.noreply.github.com>
2023-11-28 11:04:14 -0600

    Update GroupProject.py

--- GroupProject.py
+++ GroupProject.py
@@ -10,3 +10,15 @@ import sys
 
 
 
+CREATE TABLE   tableName 
+        ( UserID varchar(255),
+        Email varchar(255), 
+        Password varchar(255),
+        FirstName varchar(255),
+        LastName varchar(255),
+        Address varchar(255),
+        City varchar(255),
+        State varchar(255),
+        Zip varchar(255), 
+        Payment varchar(255), 
+        PRIMARY KEY (UserID) );
