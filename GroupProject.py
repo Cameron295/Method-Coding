8b39c60fc022582e88b51c014072a3078fb54dba
Micah George <149753394+GeorgeMicah24@users.noreply.github.com>
2023-11-29 16:15:13 -0600

    Update GroupProject.py

--- GroupProject.py
+++ GroupProject.py
@@ -4,43 +4,43 @@ import sys
 
 #main
 while True:
-    print("1.login\n2.create account\n3.logout")
+    print("1.Login\n2.Create account\n3.Logout")
     choice = str(input())
     if choice == "1":
         #Login function
-        print("logged in")
+        print("Logged in.")
         #Main Menu
         while True:
-            print("1.logout\n2.account\n3.inventory\n4.cart")
+            print("1.Logout\n2.Account\n3.Inventory\n4.Cart")
             choice2 = str(input())
             if choice2 == "1":
                 #Logout
-                print("logging out")
+                print("Logging out...")
                 break
             elif choice2 == "2":
                 #View Account Information
-                print("account info")
+                print("Account Info")
             elif choice2 == "3":
                 print("1.Go Back\n2.View Inventory\n3.Search Inventory")
                 choice3 = str(input())
                 if choice3 == "1":
-                    print("Returning to main menu")
+                    print("Returning to main menu...")
                 elif choice3 == "2":
                     #View Inventory
-                    print("viewing inventory")
+                    print("Viewing inventory...")
                 elif choice3 == "3":
                     #Search Inventory
-                    print("searching inventory")
+                    print("Searching inventory...")
                 else:
-                    print("invalid menu option, returning to main menu")
+                    print("Invalid menu option, returning to main menu...")
             elif choice2 == "4":
                 print("1.Go Back\n2.View Cart\n3.Add to Cart\n4.Remove from Cart\n5.Check Out")
                 choice3 = str(input())
                 if choice3 == "1":
-                    print("Returning to main menu")
+                    print("Returning to main menu...")
                 elif choice3 == "2":
                     #View Cart
-                    print("viewing cart")
+                    print("Viewing cart...")
                 elif choice3 == "3":
                     #Add to Cart
                     connection = sqlite3.connect("bookstore.db")
@@ -68,27 +68,27 @@ while True:
                          cart.removeFromCart(USERID HERE, x[0][0])
                          print("%s was removed from your cart." % (x[0][1]))
                     except:
-                         print("That book is not in your cart")
+                         print("That book is not in your cart.")
                     cursor.close()
                     connection.close()
                 elif choice3 == "5":
                     #Checkout
-                    print("checking out")
+                    print("Checking out...")
                 else:
-                    print("invalid menu option, returning to main menu")
+                    print("Invalid menu option, returning to main menu...")
             else:
-                print("invalid option try again")
+                print("Invalid option. Try again.")
     elif choice == "2":
         #Account Creation
-        print("account created")
+        print("Account created.")
         while True:
             print("")
     elif choice == "3":
         #Logout
-        print("logged out")
+        print("Logged out.")
         break
     else:
-        print("Invalid option try again")
+        print("Invalid option. Try again.")
 
 
        
