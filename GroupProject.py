5c9eda8aa064113e9816bd4b485a6cd6b627ad93
MackKJ <k.mackjr2002@gmail.com>
2023-11-28 18:08:47 -0600

    database

--- GroupProject.py
+++ GroupProject.py
@@ -28,30 +28,4 @@ while menu
                 print ("/n Not a Valid Choice Try agin")
 
 
-
-
-
-
-CREATE TABLE   User 
-        ( UserID varchar(255),
-        Email varchar(255), 
-        Password varchar(255),
-        FirstName varchar(255),
-        LastName varchar(255),
-        Address varchar(255),
-        City varchar(255),
-        State varchar(255),
-        Zip varchar(255), 
-        Payment varchar(255), 
-        PRIMARY KEY (UserID) );
-
-CREATE TABLE   Inventory 
-        ( ISBN varchar(255),
-        Title varchar(255), 
-        Author varchar(255),
-        Genre varchar(255),
-        Pages varchar(255),
-        ReleaseDate varchar(255),
-        Stock int,
-        PRIMARY KEY (ISBN) );
        
