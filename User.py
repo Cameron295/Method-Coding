ce9ada78e6ff761d4e5b8cce775050903e389ef8
Cameron295 <149431527+Cameron295@users.noreply.github.com>
2023-11-29 22:16:06 -0600

    Update User.py

--- User.py
+++ User.py
@@ -10,7 +10,7 @@ class User:
         self.tableName = tableName
         self.loggedIn = False
         self.userID = ""
-        connection = sqlite3("databasename")
+        connection = sqlite3.connect("databasename")
         cursor = connection.cursor()
         cmd= "CREATE TABLE IF NOT EXISTS "+ tableName + """ 
         ( UserID varchar(255),
@@ -36,6 +36,8 @@ class User:
         self.userID = ""
         
     def login():
+        connection = sqlite3.connect("databasename")
+        cursor = connection.cursor()
         #get username and password from user
         username= input("UserID: ")
         password= input("Password: ") 
@@ -62,6 +64,8 @@ class User:
         return False
 
     def viewAccountInformation():
+        connection = sqlite3.connect("databasename")
+        cursor = connection.cursor()
         # view all in table with useID in SQL
          Username= input("UserID: ")
 
@@ -73,6 +77,8 @@ class User:
          """
         cursor.execute(cmd, (self.tableName,User, Username))
     def createAccount():
+        connection = sqlite3.connect("databasename")
+        cursor = connection.cursor()
         #will insert new info into table SQL
 
         UserID = input ("UserID: ")
